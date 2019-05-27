a7396bc88c068ca1e90d15842e1e08c077ad7ffa
BartoszCzernal <bartekczernal@gmail.com>
2019-05-27 12:15:33 +0200

    lab1-zad1.3 - zamienilem implements runnable na extends thread przy naglowkach klas

---
+++ lab1/Watki1/build/classes/watki1/Watek_1.rs
@@ -0,0 +1,4 @@
+watki1.Watek2
+watki1.Watek1
+watki1.Watek_1
+watki1.Wspolny_zasob
---
+++ lab1/Watki1/src/watki1/Watek_1.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package watki1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kruczkiewicz
+ */
+class Wspolny_zasob {
+
+    ArrayList<String> komunikaty = new ArrayList<>();
+
+    synchronized void dodaj(String nowy) {
+        try {
+            if (komunikaty.size() > 0) {
+                System.out.println("bwait1");
+                wait();
+                System.out.println("await1");
+            }
+            komunikaty.add(nowy);
+            System.out.println(komunikaty.toString());
+            Thread.sleep(3000);
+        } catch (Exception e) {
+        }
+    }
+
+    synchronized void usun() {
+        try {
+            if (komunikaty.size() > 0) {
+                komunikaty.clear();
+                System.out.println("bnotify");
+                notify();
+            }
+        } catch (Exception e) {
+        }
+    }
+}
+
+class Watek1 implements Runnable {
+
+    int i = 0;
+    Wspolny_zasob p1;
+
+    public Watek1(Wspolny_zasob p) {
+        p1 = p;
+    }
+
+    public void run() {
+        while (true) {
+            p1.dodaj("nowy" + (++i));
+        }
+
+    }
+}
+
+class Watek2 implements Runnable {
+
+    Wspolny_zasob p1;
+
+    public Watek2(Wspolny_zasob p) {
+        p1 = p;
+    }
+
+    public void run() {
+        while (true) {
+            p1.usun();
+        }
+    }
+}
+
+public class Watek_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Wspolny_zasob w1 = new Wspolny_zasob();
+        Thread wt1 = new Thread(new Watek1(w1));
+        wt1.start();
+        Thread wt2 = new Thread(new Watek2(w1));
+        wt2.start();
+
+    }
+}
